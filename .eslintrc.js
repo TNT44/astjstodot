768e27f1a26fd717f17003f6da7d7cf0f4bba43d
duarte_terencio <dterencio@gmail.com>
2019-12-04 10:06:03 +0100

    fichier de conf

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": false
+    },
+    "ecmaVersion": 6
+  },
+  "rules": {
+    "indent": [
+      "off"
+    ],
+    "linebreak-style": [
+      "off",
+      "windows"
+    ],
+    "quotes": [
+      "off",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-console": "off"
+  }
+};
